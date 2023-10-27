eb9ee46547098e464cdc10f0062c2bcdc194fa23
Ivan <zakharovivann@gmail.com>
2023-10-27 20:05:24 +0300

    rewrite main.c

--- src/03/main.c
+++ src/03/main.c
@@ -1,13 +1,20 @@
-#include <fcgiapp.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcgiapp.h>
 
-int main() {
-    while (FCGI_Accept() >= 0) {
-        printf("Content-Type: text/html\r\n\r\n");
-        printf("<html><head><title>Hello World</title></head><body>");
-        printf("<h1>Hello World!</h1>");
-        printf("</body></html>");
-    }
+int main(int argc, char *argv[]) {
+  // Инициализируем структуру приложения FastCGI
+  FCGX_Request request;
+  FCGX_Init();
 
-    return 0;
-}
+  // Обрабатываем запрос
+  if (FCGX_InitRequest(&request, 0, 0) == 0) {
+    // Отправляем ответ
+    FCGX_FPrint(&request, "Content-type: text/html\r\n\r\n");
+    FCGX_FPrint(&request, "<html><body><h1>Hello World!</h1></body></html>");
+    FCGX_FinishRequest(&request);
+  }
+
+  // Выходим
+  FCGX_Shutdown();
+  return 0;
